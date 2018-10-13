5f4bbd581e6c9794e837ef02b78bfe8fd140ebac
Chinmay Garg <chinmaygarg@me.com>
2018-10-12 18:27:18 -0700

    Added working comment tree
    show, add and view feature on each post

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,37 @@
+class CommentsController < ApplicationController
+  before_action :find_commentable
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = @commentable.comments.new comment_params
+
+    if @comment.save
+      redirect_back fallback_location: root_path, notice: 'Your comment was successfully posted!'
+    else
+      redirect_back fallback_location: root_path, notice: "Your comment wasn't posted!"
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_back fallback_location: root_path, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+  
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def find_commentable
+    @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+    @commentable = Post.find_by_id(params[:post_id]) if params[:post_id]
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_043800) do
+ActiveRecord::Schema.define(version: 2018_10_13_003523) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "commentable_id"
+    t.string "commentable_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
