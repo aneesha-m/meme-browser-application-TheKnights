c3125b3454562170eb7018e61939e18b5e2473f1
Navjot Singh <jotdeepgahla@hotmail.com>
2018-10-13 16:34:27 -0700

    Added functionality to add, see, delete and edit tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+	has_many :taggings, dependent: :destroy
+	has_many :tags, through: :taggings
+
+	def all_tags=(names)
+		# names can look like this "cute, animal, dog"
+		self.tags = names.split(',').map do |name|
+			Tag.where(name: name).first_or_create!
+		end
+	end
+
+	def all_tags
+		tags.map(&:name).join(", ")
+	end
 end
