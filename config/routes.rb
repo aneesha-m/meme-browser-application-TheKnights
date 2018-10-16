c80079e5fe5d4987768c4b288fcb20acd65e8730
freddysouth <fredd-sormo-1995@hotmail.com>
2018-10-16 12:58:50 -0700

    User sessions manually implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  resources :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
-
   resources :posts
-  
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login' 
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
