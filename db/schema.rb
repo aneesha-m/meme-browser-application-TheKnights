7ca8e8d0cd5d9977c875c9b3a6cb4524172a14ce
freddysouth <fredd-sormo-1995@hotmail.com>
2018-10-16 11:38:11 -0700

    Make it possible to register user, using bcrypt to encrypt passwords

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_043800) do
+ActiveRecord::Schema.define(version: 2018_10_12_222504) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
