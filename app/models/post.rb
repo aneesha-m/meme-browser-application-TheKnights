0d1bc030103a827f3ef24da890eda9eafcbbcdf0
Aneesha <aneeshamathur@ucsb.edu>
2018-10-19 16:40:06 -0700

    Aneesha voting (#12)
    * Voting feature
    
    Added voting feature using act_as_votable gem
    1. User can upvote a post
    2. User can downvote a post
    3. We can see the number of upvotes and downvotes on a post on the listing page.
    4. We can see the number of upvotes and downvotes on individual post's page.
    
    * Corrected line I had written wrongly during conflict resolution
    
    * "Corrected line I had written wrongly during conflict resolution"
    
    * Addressed review comments
    
    Added temporary arrows.
    Put votes near the image.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
-
 	belongs_to :user, optional: true
 	has_many :taggings, dependent: :destroy
 	has_many :tags, through: :taggings
+
 	has_many :comments, as: :commentable
+
+	acts_as_votable
 
 	def all_tags=(names)
 		# names can look like this "cute, animal, dog"
@@ -15,4 +17,5 @@ class Post < ApplicationRecord
 	def all_tags
 		tags.map(&:name).join(", ")
 	end
+
 end
