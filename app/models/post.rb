e0e8759829d51ee1dba418c4553e7a0c35b280b0
Chinmay Garg <chinmaygarg@me.com>
2018-10-19 17:01:51 -0700

    Revert "Aneesha voting (#12)"
    This reverts commit 0d1bc030103a827f3ef24da890eda9eafcbbcdf0.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,9 @@
 class Post < ApplicationRecord
+
 	belongs_to :user, optional: true
 	has_many :taggings, dependent: :destroy
 	has_many :tags, through: :taggings
-
 	has_many :comments, as: :commentable
-
-	acts_as_votable
 
 	def all_tags=(names)
 		# names can look like this "cute, animal, dog"
@@ -17,5 +15,4 @@ class Post < ApplicationRecord
 	def all_tags
 		tags.map(&:name).join(", ")
 	end
-
 end
