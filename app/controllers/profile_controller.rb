6129dadc4f747e1d2aeb03b4e28e93cec2f9b587
freddysouth <fredd-sormo-1995@hotmail.com>
2018-11-27 13:49:27 -0800

    Fix bug concerning pagination with the posts on myposts

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,6 @@
 class ProfileController < ApplicationController
   before_action :authenticate_user!
   def index
-  	@posts = current_user.posts
+  	@posts = current_user.posts.paginate(page: params[:page], per_page: 5).order('created_at DESC')
   end
 end
